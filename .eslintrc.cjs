5e2bbc5392afdcecfba940fc920b47a50d7c4a08
Nemanja Manojlovic <manojlovic911@hotmail.com>
2023-08-01 19:08:53 +0200

    Add eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,30 @@
 module.exports = {
   root: true,
+  // Run environments
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  // Parser Configuration
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname,
+  },
+  // Installed Plugins
+  plugins: ["react-refresh", "@typescript-eslint"],
+  // Code Expectations/rules
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+
+  ignorePatterns: ["dist", ".eslintrc.cjs", ".devcontainer", ".github"],
+};
